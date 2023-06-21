f25ed5a2d7971ca363127706ca51a818c3052a69
Krystian Włodarczyk <87566671+kwlodarczyk0@users.noreply.github.com>
2023-06-21 10:27:20 +0200

    FT-22 (#7)

---
+++ WealthApi/Contracts/EditProfileDTO.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace WealthApi.Contracts
+{
+    public class EditProfileDTO
+    {
+        [JsonProperty]
+        [Required]
+        public string Username { get; set; }
+
+        [JsonProperty]
+        [Required]
+        public string Email { get; set; }
+
+    }
+}
---
+++ WealthApi/Contracts/NewGoalDTO.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+using WealthApi.Core.Enums;
+
+namespace WealthApi.Contracts
+{
+    public class NewGoalDTO
+    {
+        [JsonProperty]
+        [Required]
+        public string Name { get; set; }
+
+        [JsonProperty]
+        [Required]
+        public int Value { get; set; }
+
+        [JsonProperty]
+        [Required]
+        public Priority Priority { get; set; }
+
+        [JsonProperty]
+        [Required]
+        public string Deadline { get; set; }
+
+    }
+}
